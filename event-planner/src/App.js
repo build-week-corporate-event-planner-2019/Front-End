8fa9418ccacf0f017e3bf87fcbd389e38a15abb9
Kenna <kennaalastair@gmail.com>
2019-09-23 11:17:51 -0700

    Login form

--- event-planner/src/App.js
+++ event-planner/src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import { Route, Switch } from "react-router-dom";
+import Login from "./components/Login";
+// import PrivateRoute from './components/PrivateRoute';
 
 function App() {
   return (
     <div className="App">
-      
+      {/* <Switch>
+        <PrivateRoute exact path="/protected" component={BubblePage} />
+        <Route path="/login" component={Login} />
+      </Switch> */}
+      <Login />
     </div>
   );
 }
