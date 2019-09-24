e78aec457d244359717d93ba576df503dab496fc
Joe Denson <joedenson79@gmail.com>
2019-09-24 10:55:36 -0700

    pulled from master

--- event-planner/src/components/EventsPage.js
+++ event-planner/src/components/EventsPage.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import Event from
+import Event from './Event';
 
-    function EventsPage() {
+function EventsPage() {
 
 
-        return (
-            <div className="events-page">
-                <h1>Events</h1>
-                <h2>Upcoming Events</h2>
+    return (
+        <div className="events-page">
+            <h1>Events</h1>
+            <h2>Upcoming Events</h2>
+            <Event />
+        </div>
 
-            </div>
-
-        )
-    }
+    )
+}
 
 
 
