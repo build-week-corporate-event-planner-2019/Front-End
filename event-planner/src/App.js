ba1c574031afdbcdb5192dbc282b9625fdd34fba
Joe Denson <joedenson79@gmail.com>
2019-09-24 10:21:45 -0700

    formatted structure

--- event-planner/src/App.js
+++ event-planner/src/App.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import './App.css';
-
 import { Route, Switch } from "react-router-dom";
 import Login from "./components/Login";
 // import PrivateRoute from './components/PrivateRoute';
+import Navigation from './components/Navigation';
+import EventInfoBlock from './components/EventInfoBlock';
+import EventMainSection from './components/EventMainSection';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
+      <Navigation />
+      <EventInfoBlock />
+      <EventMainSection />
+      <Footer />
       {/* <Switch>
         <PrivateRoute exact path="/protected" component={BubblePage} />
         <Route path="/login" component={Login} />
       </Switch> */}
-      <Login />
+      {/* <Login /> */}
     </div>
   );
 }
