365bb4d29e8f3c8e5029eac092136e904b22c681
Joe Denson <joedenson79@gmail.com>
2019-09-27 10:52:11 -0700

    styled the event home page

--- event-planner/src/components/EventAdd.js
+++ event-planner/src/components/EventAdd.js
@@ -7,7 +7,7 @@ class AddEvent extends React.Component {
   // when you have handled the token, navigate to the BubblePage route
   state = {
     eventinfo: {
-      user_id: 0, 
+      user_id: 0,
       name: "",
       description: "",
       start_date: "",
@@ -32,12 +32,12 @@ class AddEvent extends React.Component {
     this.props.addEvent(this.state.eventinfo);
     this.setState({
       eventinfo: {
-      name: "",
-      description: "",
-      start_date: "",
-      end_date: "",
-      location: "",
-      budget: ""
+        name: "",
+        description: "",
+        start_date: "",
+        end_date: "",
+        location: "",
+        budget: ""
       }
     })
     this.props.history.push('/events');
@@ -54,55 +54,67 @@ class AddEvent extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className="event-home">
         <form onSubmit={this.addEvent}>
-          <label>Event Name: </label>
-          <input
-            type="text"
-            name="name"
-            value={this.state.eventinfo.name}
-            onChange={this.handleChange}
-          />
+          <div className="event-name-field">
+            <label>Event Name: </label>
+            <input
+              type="text"
+              name="name"
+              value={this.state.eventinfo.name}
+              onChange={this.handleChange}
+            />
+          </div>
           <br />
-          <label>Event Description: </label>
-          <input
-            type="text"
-            name="description"
-            value={this.state.eventinfo.description}
-            onChange={this.handleChange}
-          />
+          <div className="event-description-field">
+            <label>Event Description: </label>
+            <input
+              type="text"
+              name="description"
+              value={this.state.eventinfo.description}
+              onChange={this.handleChange}
+            />
+          </div>
           <br />
-          <label>Event Start: </label>
-          <input
-            type="text"
-            name="start_date"
-            value={this.state.eventinfo.start_date}
-            onChange={this.handleChange}
-          />
+          <div className="event-start-field">
+            <label>Event Start: </label>
+            <input
+              type="text"
+              name="start_date"
+              value={this.state.eventinfo.start_date}
+              onChange={this.handleChange}
+            />
+          </div>
           <br />
-          <label>Event End: </label>
-          <input
-            type="text"
-            name="end_date"
-            value={this.state.eventinfo.end_date}
-            onChange={this.handleChange}
-          />
+          <div className="event-end-field">
+            <label>Event End: </label>
+            <input
+              type="text"
+              name="end_date"
+              value={this.state.eventinfo.end_date}
+              onChange={this.handleChange}
+            />
+          </div>
           <br />
-          <label>Location: </label>
-          <input
-            type="text"
-            name="location"
-            value={this.state.eventinfo.location}
-            onChange={this.handleChange}
-          />
+          <div className="event-location-field">
+            <label>Location: </label>
+            <input
+              type="text"
+              name="location"
+              value={this.state.eventinfo.location}
+              onChange={this.handleChange}
+            />
+          </div>
           <br />
-          <label>Budget: </label>
-          <input
-            type="text"
-            name="budget"
-            value={this.state.eventinfo.budget}
-            onChange={this.handleChange}
-          />
+          <div className="event-budget-field">
+            <label>Budget: </label>
+            <input
+              type="text"
+              name="budget"
+              value={this.state.eventinfo.budget}
+              onChange={this.handleChange}
+            />
+          </div>
         </form>
         <button onClick={() => this.addEvent()} className="add-btn">Add Event</button>
       </div>
